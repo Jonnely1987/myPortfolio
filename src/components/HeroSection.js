4d1fb133679b96484388958522c8913a3bffd83f
Jonnely1987 <120239656+Jonnely1987@users.noreply.github.com>
2023-05-09 11:09:21 +0800

    Add files via upload

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,14 +7,14 @@ function HeroSection() {
   return (
     <div className='hero-container'>
         <video src='/public/videos/video-2.mp4' autoPlay loop muted />
-        <h1>ADVENTURE AWAITS</h1>
-        <p>What are you waiting for?</p>
+        <h1>Hi I'm Jonnely</h1>
+        <p>A Web Developer</p>
         <div classname="hero-btns">
         <Button className="btns" buttonStyle={'btn--outline'} buttonSize={'btn--large'}>
-            GET STARTED
+         SEE MY WORK
         </Button>
         <Button className="btns" buttonStyle={'btn--primary'} buttonSize={'btn--large'}>
-            WATCH TRAILER <i className='far fa-play-circle' />
+         DOWNLOAD MY CV
         </Button>
 
 
